d78d8313cbea02e1ae157ac661b7e15ddc33e0df
salvation27 <nikolayzalipykin@gmail.com>
2022-04-01 16:40:36 +0300

    change url API to Heroku

--- src/store/productsApi.js
+++ src/store/productsApi.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const productsApi = createApi({
     reducerPath: "productsApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+    // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://shop-toolkit.herokuapp.com/" }),
     endpoints: (builder) => ({
       getAllProducts: builder.query({
         query: () => `products`,
